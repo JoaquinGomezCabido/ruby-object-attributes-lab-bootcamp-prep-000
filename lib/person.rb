3928547189d5675ae5fabe1ea76807a68f7fb657
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-15 19:11:25 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(set_name)
+    @person_name = set_name
+  end
+
+  def name
+    @person_name
+  end
+
+  def job=(set_job)
+    @person_job = set_job
+  end
+
+  def job
+    @person_job
+  end
+
+end
