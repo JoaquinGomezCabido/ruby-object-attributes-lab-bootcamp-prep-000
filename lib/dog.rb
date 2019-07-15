7acffcee45458636b1546fc1d58d48b1ae313dde
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-15 19:10:12 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(set_name)
+    @dog_name = set_name
+  end
+
+  def name
+    @dog_name
+  end
+
+  def breed=(set_breed)
+    @dog_breed = set_breed
+  end
+
+  def breed
+    @dog_breed
+  end
+
+end
